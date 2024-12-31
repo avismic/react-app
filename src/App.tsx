76a54e85fd0aca76f9425fb0e714f0b88aec93fc
abhishek <abhishekanandsm0311@gmail.com>
2024-12-31 21:01:55 +0530

    hide and show alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Alert from "./Alert";
 import SuccessButton from "./SuccessButton";
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
+      {alertVisible && (
+        <Alert
+          onClose={() => {
+            setAlertVisibility(false);
+          }}
+        >
+          My Alert
+        </Alert>
+      )}
       <SuccessButton
         color="danger"
         onClick={() => {
-          console.log("hello");
+          setAlertVisibility(true);
         }}
       >
         My Button
